050d28c813526d0ed593a1e4ff872bd1a36e85ed
Harvey Yang <harveyMac@YangdeMBP.lan>
2016-03-07 16:41:54 +0800

    1

---
+++ testMyJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testMyJenkins
+//
+//  Created by Harvey Yang on 16/3/7.
+//  Copyright © 2016年 Harvey Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testMyJenkins/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testMyJenkins
+//
+//  Created by Harvey Yang on 16/3/7.
+//  Copyright © 2016年 Harvey Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
